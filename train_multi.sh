2bf7babb47e75ed2e2520d11493ddd5bd59bf3ae
Lannie Dalton Hough <58961576+ldhough@users.noreply.github.com>
2026-03-10 07:30:25 -0400

    Nairr tut (#22)
    * updated for nairr tut
    
    * readme typos fixed
    
    * vllm serve updated

--- train_multi.sh
+++ train_multi.sh
@@ -4,13 +4,13 @@
 #SBATCH -t 00:05:00
 #SBATCH -p gpu
 #SBATCH --gres=gpu:a100:4
-#SBATCH -A sc25-aac
+#SBATCH -A nairr-class
 #SBATCH --exclusive
 #SBATCH --mem=500G
-#SBATCH --reservation=sc25
+#SBATCH --reservation=nairr
 
 
-export SCRATCH="/scratch/zt1/project/sc25/shared/"
+export SCRATCH="/scratch/zt1/project/nairr/shared/"
 export HF_HOME="${SCRATCH}/.cache/huggingface"
 export HF_TRANSFORMERS_CACHE="${HF_HOME}"
 export HF_DATASETS_CACHE="${HF_HOME}/datasets"
--- train_single.sh
+++ train_single.sh
@@ -4,12 +4,12 @@
 #SBATCH -t 00:05:00
 #SBATCH -p gpu
 #SBATCH --gres=gpu:a100:1
-#SBATCH -A sc25-aac
+#SBATCH -A nairr-class
 #SBATCH --mem=100G
-#SBATCH --reservation=sc25
+#SBATCH --reservation=nairr
 
 
-export SCRATCH="/scratch/zt1/project/sc25/shared/"
+export SCRATCH="/scratch/zt1/project/nairr/shared/"
 export HF_HOME="${SCRATCH}/.cache/huggingface"
 export HF_TRANSFORMERS_CACHE="${HF_HOME}"
 export HF_DATASETS_CACHE="${HF_HOME}/datasets"
